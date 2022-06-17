f1dd86ab0505dd7c37f52321241751c5611ec0c1
TqAA <boev.maxim042@gmail.com>
2022-06-17 10:19:10 +0300

    Added input-output text file

--- main.c
+++ main.c
@@ -3,173 +3,194 @@ Author: Boev Maxim
  Calculator with 6 basic functions
  The user must select one of the allowable operations and 2 numbers, then the calculator will make calculations and prompt the user to continue or stop working
 */
+
 #define _CRT_SECURE_NO_WARNINGS
+//Standard libraries
 #include <stdio.h>
 #include <stdlib.h>
-int main (void)
+//Function for degree expansion
+double degree(float num1, float num2)
 {
-    printf (" Hello!\n");
-    printf (" This program performs the selected operation between two numbers \n" );
-    printf (" To be successful, you must first enter the operation and then the 2 numbers [ 1 in the case of the factorial] \n" );
-    printf (" Available operations :addition(+), subtraction(-), multiplication(*), division(/ ), exponentiation(^), factorial(!), vector(v) \n" );
-    
-    // Entering variables
-    double num1, num2; // Numbers entered by the user from the keyboard
-    double res; // The result of the operation
-    char operation; // Type of operation
-    char operationv;
-    char consent; // Reset to resume operation after the end of the program (if the user wishes)
-    double *A, *B; //
-    int size; 
-    double resv; //
-    do // Cycle for resuming work after the end of the program
-    {
-        setvbuf (stdout, NULL, _IONBF, 0);
-        setvbuf (stderr, NULL, _IONBF, 0);
-        // Choice of operation
-        printf ("\n Select an operation ( +, -, *, /, ^, !):");
-        scanf (" %c", &operation);
-        
-        // operation
-        switch (operation)
-        {
-        case 'v': // Operations with vectors
-            printf("Enter the size of the vectors: ");
-            scanf("%i", &size);
-            A = malloc(size * sizeof(int));
-            B = malloc(size * sizeof(int));
-            printf("Enter the coordinates of the first vector: ");
-            for (int i = 0; i < size; i++) scanf("%lf", &A[i]);
-            printf("Enter the coordinates of the second vector: ");
-            for (int i = 0; i < size; i++) scanf("%lf", &B[i]);
-            printf("Enter one of the provided operations for vectors: (+),(-),(*): ");
-            scanf(" %c", &operationv);
-            switch (operationv)
-            {
-            case '+': // Addition operation with vectors
-                printf("Addition of vectors: ");
-                for (int i = 0; i < size; i++) printf("%lf ", A[i] + B[i]);
-                printf("\n");
-                break;
-            case '-': // Subtraction operation with vectors
-                printf("Subtracting vectors: ");
-                for (int i = 0; i < size; i++) printf("%lf ", A[i] - B[i]);
-                printf("\n");
-                break;
-            case '*': // Scalar product operation with vectors
-                printf("Scalar product of vectors: ");
-                for (int i = 0; i < size; i++) printf("%lf ", A[i] * B[i]);
-                printf("\n");
-                break;
-            default: // In the case of an incorrectly selected operation
-                printf("Error 404!");
-            }
-            free(A); // Releasing used memory
-            free(B);
-            break;
-            // Function for adding numbers
-        case '+':
-            printf ("\nEnter the first number:");
-            scanf (" %lf", &num1);
-            printf ("\nEnter the second number:");
-            scanf (" %lf", &num2);
-            res = num1 + num2;
-            printf ("\n%lf + %lf = %lf", num1, num2, res);
-            break;
+	if (num2 == 0) return 1;
+	else
+	{
+		float numb1 = num1;
+		for (int i = 1; i < num2; i++)
+		{
+			numb1 *= num1;
+		}
+		return numb1;
+	}
+}
 
-            // Function for subtracting numbers
-        case '-':
-            printf ("\nEnter the first number:");
-            scanf (" %lf", &num1);
-            printf ("\nEnter the second number:");
-            scanf (" %lf", &num2);
-            res = num1 - num2;
-            printf ("\n%lf - %lf = %lf", num1, num2, res);
-            break;
+//Function for vector operations
+void vectorCalculation(char operations, FILE* inFile, FILE* outFile)
+{
+	int* vector1;
+	int* vector2;
+	float scalar;
+	int vectorSize;// The dimensionality of the vector
 
-            // Function for multiplying numbers
-        case '*':
-            printf ("\nEnter the first number:");
-            scanf (" %lf", &num1);
-            printf ("\nEnter the second number:");
-            scanf (" %lf", &num2);
-            res = num1 * num2;
-            printf ("\n%lf * %lf = %lf", num1, num2, res);
-            break;
+	fscanf(inFile, "%i", &vectorSize);
 
-            // Function for dividing numbers
-        case '/':
-            printf ("\nEnter the first number:");
-            scanf (" %lf", &num1);
-            printf ("\nEnter the second number  :");
-            scanf (" %lf", &num2);
-            res = num1 / num2;
-            if (num2 == 0) printf ("Делить на ноль нельзя!");
-            else printf ("\n%lf / %lf = %lf", num1, num2, res);
-            break;
+	vector1 = malloc(vectorSize * sizeof(int));
+	vector2 = malloc(vectorSize * sizeof(int));
 
-            // A function for the exponentiation of a number
-        case '^':
-            printf ("\nEnter the first number:");
-            scanf (" %lf", &num1);
-            printf ("\nEnter the second number:");
-            scanf (" %lf", &num2);
-            double numb1 = num1;
-            if (num2 == 0) {
-                printf ("\n %lf^%lf= 1", num1, num2);
-            }
-            else if (num2 == 1) {
-                printf ("\n %lf^%lf= %lf", num1, num2, num1);
-            }
-            else if (num2 == -1) {
-                res = 1 / num1;
-                printf ("\n %lf^%lf=%lf", num1, num2, res);
-            }
-            else if (num2 < 0) {
-                num2 *= -1;
-                for (int i = 1; i < num2; i++)
-                {
-                    num1 = num1 * numb1;
-                    res = 1/num1;
-                }
-                printf ("\n%lf ^ %lf = %lf", numb1, num2, res);
-            }
-            else if (num2>0) {
+	for (int i = 0; i < vectorSize; i++) fscanf(inFile, "%i", &vector1[i]);
 
-                for (int i = 1; i < num2; i++)
-                {
-                    num1 = num1 * numb1;
-                    res = num1;
-                }
-                printf ("\n%lf ^ %lf = %lf", numb1, num2, res);
-            }
-            
-            break;
+	for (int i = 0; i < vectorSize; i++) fscanf(inFile, "%i", &vector2[i]);
 
-            // Function for finding the factorial of a number 
-        case '!':
-            printf ("\nEnter the number:");
-            scanf (" %lf", &num1);
-            double numv1 = num1;
-            if (num1 > 0) for (int i = 1; i < numv1; i++)
-            {
-                num1 = num1 * (numv1 - i);
-            }
-            if (num1 == 0) printf ("0! = 1");
-            else if (num1 < 0) printf ("Undefined");
-            else printf ("\n%lf! = %lf", numv1, num1);
-            break; 
-         // In the case of an incorrectly selected operation
-        default:
-            printf("\nOperation selection error");
-        }
-        // Consent/refusal to continue working with the program
-        printf ("\nWould you like to continue? Yes=y,No=n:");
-        scanf (" %c", &consent);
-        // Continuation condition
-    } while (consent == 'y');
-    // Condition of completion
-    if (consent == 'n') printf("\nGoodbye!");
-    else printf ("\nOperation selection error");
-    return 0;
+	void printVector(int* vector)
+	{
+		fprintf(outFile, "(");
+		for (int i = 0; i < (vectorSize - 1); i++) fprintf(outFile, "%i, ", vector[i]);
+		fprintf(outFile, "%i)", vector[vectorSize - 1]);
+	}
+
+	printVector(vector1);
+	fprintf(outFile, " %c ", operations);
+
+	printVector(vector2);
+	fprintf(outFile, " = ");
+
+	int result[vectorSize]; //to store the result in vector form
+
+	switch (operations)
+	{
+	case '+':
+		for (int i = 0; i < vectorSize; i++) result[i] = vector1[i] + vector2[i];
+		printVector(result);
+		fprintf(outFile, "\n");
+		break;
+	case '-':
+		for (int i = 0; i < vectorSize; i++) result[i] = vector1[i] - vector2[i];
+		printVector(result);
+		fprintf(outFile, "\n");
+		break;
+	case '^':
+		for (int i = 0; i < vectorSize; i++) scalar += vector1[i] * vector2[i];
+		fprintf(outFile, "%g\n", scalar);
+		break;
+	default:
+		fprintf(outFile, "Error404\n", operations);
+
+	}
+	//Freeing the memory
+	free(vector1);
+	free(vector2);
+}
+//Function for determining the factorial
+long long int factorial(int num1)
+{
+	if (num1 == 0) return 1;
+	else
+	{
+		long long int fact = 1;
+		for (int i = 1; i <= num1; i++)
+		{
+			fact *= i;
+		}
+		return fact;
+	}
+}
+
+//Function for simple arithmetic operations
+void arithmetic(char operations, FILE* inFile, FILE* outFile)
+{
+	float num1, num2 = 1; //Variables for operands
+	
+	fscanf(inFile, "%f", &num1);
+
+	int wholeA = num1;
+	float CheckA = num1 / wholeA; //Checking for an integer
+
+	if (operations != '!')
+	{
+		fscanf(inFile, "%f", &num2);
+	}
+
+	int wholeB = num2; //Checking for an integer
+	float CheckB = num2 / wholeB;
+
+	switch (operations)
+	{
+	case '+':
+		fprintf(outFile, "%g %c %g = %g\n", num1, operations, num2, num1 + num2);
+		break;
+
+	case '-':
+		fprintf(outFile, "%g %c %g = %g\n", num1, operations, num2, num1 - num2);
+		break;
+
+	case '*':
+		fprintf(outFile, "%g %c %g = %g\n", num1, operations, num2, num1 * num2);
+		break;
+
+	case '/':
+		if (num2 == 0) fprintf(outFile, "Don't do it\n");
+		else fprintf(outFile, "%g %c %g = %g\n", num1, operations, num2, num1 / num2);
+		break;
+
+	case '!':
+		if ((CheckA != 1) || (num1 < 0)) fprintf(outFile, "Must be a non-negative integer\n");
+		else fprintf(outFile, "%g %c = %lli\n", num1, operations, factorial(num1));
+		break;
+
+	case '^':
+		if ((CheckB != 1) || (num2 < 0)) fprintf(outFile, "Degree Must be a non-negative integer\n");
+		else fprintf(outFile, "%g %c %g = %g\n", num1, operations, num2, degree(num1, num2));
+		break;
+
+	default:
+		fprintf(outFile, "Error404\n", operations);
+	}
+}
+
+
+int main(void)
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	printf("You need to write down the data in a certain order:\n");
+	printf("Operation(+,- etc), Data type(v,s), Dimension(for v),Operands (separated by a space)\n");
+
+	while (1) //Infinite while loop
+	{
+		char choice, operation, type; //type - переменная для хранения типа операндов
+		char input_Name[255];
+		char output_Name[255];//Variables for storing file names
+
+
+		printf("Enter a name for the input file: ");
+		scanf("%s", &input_Name);
+
+		printf("Enter a name for the output file: ");
+		scanf("%s", &output_Name);
+
+		FILE* input = fopen(input_Name, "r");
+		FILE* output = fopen(output_Name, "w");
+
+		while (fscanf(input, " %c %c", &operation, &type) != EOF) //Reading a file all the way through("EOF"indicator of the operating system
+		{                                                         //indicating that the data in the source has run out of)
+			if (type == 'v') vectorCalculation(operation, input, output);
+			else if (type == 's') arithmetic(operation, input, output);//Definitions of the type of operations (vector or scalar)
+			else
+			{
+				fprintf(output, "Error404\n", type);
+			}
+		}
+
+		//Closing files after the end of the run
+		fclose(input);
+		fclose(output);
+
+		//Restarting the programme
+		printf("Do you want to continue working with the calculator? (y/n) \n");
+		scanf(" %c", &choice);
+		if (choice == 'n') break;
+
+	}
+
+
+	return 0;
 }
